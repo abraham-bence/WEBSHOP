85818addaaea7d669603dab27975eb40f31f881c
Bencuska^^ <75310580+abraham-bence@users.noreply.github.com>
2024-10-10 22:52:21 +0000

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Redirect, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  
+  
   @Get()
   @Render('index')
   getHello() {
@@ -12,4 +14,21 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+  
+  @Get('shop')
+  @Render('shop.ejs')
+  getShop(){
+
+  }
+  @Post('shop')
+  postProduct(@Body() product, @Res() res:Response){
+    console.log(product.product)
+    return res.redirect("/userForm?product="+product.product);
+  }
+  @Get('/userForm')
+  @Render('userForm.ejs') 
+  getUser(){
+
+  }
+
 }
