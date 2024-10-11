f1d88550c7978508cbdbb377489041c288ab8f3f
Bencuska^^ <75310580+abraham-bence@users.noreply.github.com>
2024-10-11 13:33:48 +0000

    alap validáció

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post, Redirect, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import { orderDto } from './order.dto';
 
 @Controller()
 export class AppController {
@@ -25,10 +26,23 @@ export class AppController {
     console.log(product.product)
     return res.redirect("/userForm?product="+product.product);
   }
+
   @Get('/userForm')
   @Render('userForm.ejs') 
-  getUser(){
+  order(@Body() orderDto: orderDto , @Res() res:Response){
+    const errors: string[] = []
 
+    if(!orderDto.lastName || !orderDto.firstName){
+      errors.push('Minden mezőt kötelező megadni!')
+    }
+    if (orderDto.coupon && /^[A-Z]{2}-\d{4}$/.test(orderDto.coupon)){
+      errors.push("A kuponkód nem megfelelő formátumú!")
+    }
+    if (errors.length > 0) {
+      return res.render('userForm.ejs', {
+        errors
+      })
+    }
   }
 
 }
---
+++ src/order.dto.ts
@@ -0,0 +1,14 @@
+export class orderDto {
+    firstName: string;
+    lastName: string;
+    coupon: string;
+    
+    country: string;
+    postalCode: string;
+    city: string;
+    street: string;
+    houseNumber: number;
+    cardNumber: string;
+    expDate: string;
+    cvc: string;
+}
